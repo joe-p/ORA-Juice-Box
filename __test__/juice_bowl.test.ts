7dbd9e0794faffd7a7c2798a8d1fbac68a651e40
Joe Polny <joepolny@gmail.com>
2023-12-30 21:12:09 -0500

    Project initialised with AlgoKit CLI using template: https://github.com/algorand-devrel/tealscript-algokit-template.git

---
+++ __test__/juice_bowl.test.ts
@@ -0,0 +1,41 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { JuiceBowlClient } from '../contracts/clients/JuiceBowlClient';
+
+const fixture = algorandFixture();
+
+let appClient: JuiceBowlClient;
+
+describe('JuiceBowl', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { algod, testAccount } = fixture.context;
+
+    appClient = new JuiceBowlClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+});
